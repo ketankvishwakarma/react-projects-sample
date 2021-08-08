3e422cc47920c8cf62a2f7259bf5c0c577f81cc9
Ketan Vishwakarma <ketan.vishwakarma@inventum.net>
2021-08-08 06:17:05 +0530

    Remove item form list

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { useEffect, useState } from "react";
 const title = "Sweety!";
 
 const welcome = {
-  greeting: "Hey",
-  title: "React",
+  greeting: "Hello",
+  title: "Sweety!",
 };
 
 function App() {
-  const stories = [
+  
+  const initialStories = [
     {
       title: "React",
       url: "https://reactjs.org/",
@@ -26,37 +27,71 @@ function App() {
       objectID: 1,
     },
   ];
+  const [stories, setStories] = React.useState(initialStories);
 
-  
-  const List = ({list}) => {
-    return list.map((item) => (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    ));
-  };
-
-  const useSemiPersistentState = (key,initialState) => {
+  const useSemiPersistentState = (key, initialState) => {
     const [value, setValue] = useState(
       localStorage.getItem(key) || initialState
     );
 
     React.useEffect(() => {
       localStorage.setItem(key, value);
-    }, [value,key]);
+    }, [value, key]);
 
-    return [value,setValue]
+    return [value, setValue];
   };
-  
-  const [searchTerm,setSearchTerm] = useSemiPersistentState('search','React');
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", '');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  
+  const handleRemoveStories = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
+
+  const Item = ({ item, onRemoveItem }) => {
+    function handleRemoveItem() {
+      onRemoveItem(item);
+    };
+
+    return (
+      <div>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        &nbsp;
+        <span>written by {item.author}</span>
+        &nbsp;
+        <span>
+          has <b>{item.num_comments}</b> comments
+        </span>
+        &nbsp;
+        <span>
+          and scores <b>{item.points}</b> points
+        </span>
+        <span>
+          <button type="button" onClick={() => onRemoveItem(item)}>
+            Dismiss
+          </button>
+        </span>
+      </div>
+    );
+  };
+
+  const List = ({ list, onRemoveItem }) => {
+   return list.map(item =>(
+     <Item 
+        key={item.objectID} 
+        item={item} 
+        onRemoveItem={onRemoveItem}
+      />
+   ));
   };
 
   const searchedStories = stories.filter((story) =>
@@ -72,27 +107,48 @@ function App() {
         label="Search"
         type="text"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search: </strong>
       </InputWithLabel>
       <hr />
       &nbsp;
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStories} />
     </div>
   );
 }
 
-const InputWithLabel =({id,label,value,type,onInputChange,children})=>(
-  <>
-   {children}
-    &nbsp; &nbsp;
-    <input 
-      id={id}
-      type={type}
-      value={value}
-      onChange={onInputChange}></input>
-  </>
-);
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  isFocused,
+  type,
+  onInputChange,
+  child,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <>
+      {child}
+      &nbsp; &nbsp;
+      <input
+        id={id}
+        ref={inputRef}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      ></input>
+    </>
+  );
+};
 
 export default App;
